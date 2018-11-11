7a84d293373bbb95b8a4951b334c06572a15e1cc
Liz Spangler <lizspan@gmail.com>
2018-11-11 14:11:03 -0500

    trying sleep

--- src/main/java/org/example/kafka/consumer/ExampleConsumer.java
+++ src/main/java/org/example/kafka/consumer/ExampleConsumer.java
@@ -53,6 +53,12 @@ public class ExampleConsumer {
 			for (TopicPartition partition : partitions) {
 				log.info("Now at: {}", consumer.position(partition));
 			}
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 
